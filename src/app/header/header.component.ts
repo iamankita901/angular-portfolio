9154f1843a056b11d0aa9e553a9f370330ce0158
Ankita Gupta <iamankitagupta901@gmail.com>
2025-07-17 21:28:24 +0530

    name change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  name = 'Ankita Gupta';
+  name = 'Ankita Gupta jwnkawndkjawnkjdnkwjdnkjankdawbd';
   imagePath: string ="images/profilepic.jpg";
   title = 'Full-stack Developer';
   tagline = 'Turning ideas into responsive and beautiful web applications.';
